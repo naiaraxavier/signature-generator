a51d42794e882f0524e8d654a3f601e01091348f
Naiara Martins <naiaraxf@gmail.com>
2025-04-23 23:50:01 -0300

    fix: bug toggle theme

--- components/theme/mode-toggle.tsx
+++ components/theme/mode-toggle.tsx
@@ -19,8 +19,8 @@ export const ModeToggle = () => {
         className="relative w-10 h-10 flex items-center justify-center overflow-hidden p-0 cursor-pointer rounded-full transition-all duration-500 hover:bg-gray-200 dark:hover:bg-gray-700"
         onClick={() => setTheme(theme === "light" ? "dark" : "light")}
       >
-        <Sun className="absolute rotate-90 scale-0 opacity-0 transition-all duration-300 ease-in-out dark:rotate-0 dark:scale-100 dark:opacity-100" />
-        <Moon className="absolute transition-all duration-300 ease-in-out dark:-rotate-90 dark:scale-0 dark:opacity-0" />
+        <Sun className="absolute rotate-90 scale-0 opacity-0 transition-all duration-300 ease-in-out dark:rotate-0 dark:scale-100 dark:opacity-100 text-yellow-500" />
+        <Moon className="absolute transition-all duration-300 ease-in-out dark:-rotate-90 dark:scale-0 dark:opacity-0 text-gray-900" />
       </Button>
     </div>
   );
